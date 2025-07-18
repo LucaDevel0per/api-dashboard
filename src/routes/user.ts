940cfeda5e94c109b3fae21d5ea34ba981242edf
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-07-18 14:29:19 -0300

    feat: adicionado get single user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,9 +5,9 @@ import isAdmin from '../middlewares/isAdmin';
 
 const router = Router()
 
+// todos os users
 router.get('/users', verifyToken, isAdmin, async (req, res) => {
     try{
-
         const users = await prisma.user.findMany({
             select: {
                 id: true,
@@ -25,5 +25,29 @@ router.get('/users', verifyToken, isAdmin, async (req, res) => {
         res.status(500).json({ erro: 'Server error' })
     }
 }) 
+
+router.get('/users/:id', verifyToken, isAdmin, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                // password: true,
+                avatarUrl: true,
+                createdAt: true,
+                }
+            });
+            if (!user) {
+                return res.status(404).json({ message: 'Usuário nçao encontrado'})
+            }
+            res.json(user);
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error'})
+    }
+})
 
 export default router;
