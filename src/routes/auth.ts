254b9ba78b98016ef901eafc392bdb73ca32cc17
Luca Ribeiro lopes <lucaribeiro1242@gmail.com>
2025-07-18 11:23:41 -0300

    feat: Adicionado rota de Login com autenticação

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,15 +6,14 @@ import prisma from '../prisma/client';
 const router = Router()
 const JWT_SECRET = process.env.JWT_SECRET
 
+// Registro
 router.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
-
     try {
         const existingUser = await prisma.user.findUnique({ where: { email }});
         if (existingUser) {
             return res.status(400).json({ message: "E-mail já cadastrado." });
         }
-
         // criptografar senha
         const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -36,6 +35,38 @@ router.post('/register', async (req, res) => {
         console.log("Erro ao registar", err);
         res.status(500).json({ message: "Server Error"})
     }
+})
+
+// Login
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: { email },
+    })
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found." })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+
+    if (!password) {
+        return res.status(401).json({ message: "Senha incorreta." })
+    }
+
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+        },
+        JWT_SECRET as string,
+        {
+            expiresIn: '1d'
+        }
+    )
+
+    res.json({ token })
 })
 
 export default router;
